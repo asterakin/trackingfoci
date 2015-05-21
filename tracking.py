2750673d1a42aaddfcf3d1bf9780e81914eebb7f
asterakin <styl.stella@Gmail.com>
2015-05-21 16:09:15 -0700

    lets begin

---
+++ tracking.py
@@ -0,0 +1,11 @@
+__author__ = 'Stella'
+
+
+''''import numpy as np, h5py
+f = h5py.File('Cell0000620.mat','r')
+data = f.get('data/variable1')
+data = np.array(data)
+'''''
+
+import scipy.io as sio
+matcontents = sio.loadmat('')
