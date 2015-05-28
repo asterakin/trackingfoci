39bcaeee23ddf6df3a1da5442fff74d5b080c174
asterakin <styl.stella@Gmail.com>
2015-05-28 13:10:43 -0700

    part of simulated annealing
    cost seems to be wrong. it always calculates higher values. i am not
    sure why.

--- tracking.py
+++ tracking.py
@@ -2,9 +2,11 @@ __author__ = ['Stella', 'phil0']
 
 from scipy import misc, io
 import numpy as np
-from random import random
+from random import randint
+import matplotlib
 import matplotlib.pyplot as plt
-
+import math
+from random import random
 
 MIN_SCORE = 3
 ALLOW_SPLITS = False
@@ -21,52 +23,46 @@ def no_nans(biglist):
                 return False
     return True
 
+
+# returns spots of format
+# spots has the format Track1: [[x1,y1,score],[x2,y2,score],[x3,y4,score],... ]
+#  Track 2:  [[],[],[]]
 def convertMatFile(filename):
     celldata = io.matlab.loadmat(filename)
     global lifetime
     global elements
-    # TODO: does celldata have the key 'xx' ? I couldn't get this function to work with any of the .mat files I tried.
-    # only use simpleTrack.mat - the other cell files are the old ones that don't work yet.
 
     lifetime = len(celldata['xx'][0])
     elements = len(celldata['xx'])
-    # TODO: Please don't use the underscore as a variable name. It has a speacial python meaning - but also you just shouldn't use it. (fixed)
-    # i thought they used _ in variable names in python for some reason..
+
     spots = [list([[np.nan] for y in range(lifetime)]) for i in range(elements)]
     for spot in range(elements):
         for time in range(lifetime):
             if celldata['sc'][spot][time] > MIN_SCORE:
-                spots[spot][time] = [celldata['xx'][spot][time],celldata['yy'][spot][time],celldata['sc'][spot][time]]
+                spots[spot][time] = [celldata['xx'][spot][time], celldata['yy'][spot][time], celldata['sc'][spot][time]]
 
-    # spots has the format Track1: [[x1,y1,score],[x2,y2,score],[x3,y4,score],... ]
-    # Track 2:  [[],[],[]]
-    print(spots)
     return spots
 
-def run (filename = 'simpleTrack.mat'):
+
+def run(filename='simpleTrack.mat'):
     spots = convertMatFile(filename)
-    # create random initial state
-    # just take the spots the way they are in the table initially and act like they are a tracks
-    # TODO: is tracks supposed to be global? Do you mean deepcopy (copy and deepcopy must be imported)? (If not, why is this here?)
-    # i was just trying to copy the spots to a new list so that i have a new
-    # copy of the tracks that i will be modifying.
     tracks = spots.copy()
     tracks = initial_state(tracks)
-    [final_state,cost] = sim_anneal(tracks)
+    plt.ion()
+    plot(tracks)
+    [final_state, cost] = sim_anneal(tracks)
+    plot(final_state)
+    print("THE END")
 
-def initial_state (tracks):
-    # do nearest neighbour from both sides to find some initial tracks
-    #pick first spot
-    # Not done yet.
 
-    #newtrack = range
-    #for t in range(1,lifetime):
-		return tracks; # for now just keep the random initial state
-    #pass
+# TODO: START BY FINDING THE NEAREST NEIGHBOR
+def initial_state(tracks):
+    return tracks;  # for now just keep the random initial state
+
 
 def sim_anneal(state):
     old_cost = cost(state)
-    T = 1.0
+    T = 10.0
     T_min = 0.00001
     alpha = 0.9
     while T > T_min:
@@ -75,45 +71,79 @@ def sim_anneal(state):
             new_state = neighbor(state)
             new_cost = cost(state)
             ap = acceptance_probability(old_cost, new_cost, T)
+            print(str(old_cost) + " vs " + str(new_cost))
+
             if ap > random():
+                print("Accepted")
                 state = new_state
                 old_cost = new_cost
-            i += 1
+                plot(new_state)
+        i += 1
         T = T * alpha
-    return state, cost
 
-# evaluates cost of state
-# TODO: Please don't push unless the code actually compiles (fixed).
+    return state, cost  # TODO: calculate a neighbor state
+
+# TODO : Figure out the non-nan values and use only those
+def neighbor(state):
+    # make random change in random number of spots
+    how_many_spots = randint(0, 10)
+
+    for spots in range(how_many_spots):
+        timepoint = randint(0, lifetime - 1)
+        track1 = randint(0, 1)  # should be elements-1 but there are too many nan tracks
+        track2 = randint(0, 1)
+        # exchange track1 with track2
+        temp = state[track1][timepoint]
+        state[track1][timepoint] = state[track2][timepoint]
+        state[track2][timepoint] = temp
+
+    return state
+
+
 def cost(state):
-    # go through each track
-    distance_metric = [None] * elements
-    for track in range(len(state)):
-        # TODO: don't leave your debugging code in here when you push (fixed).
-        for time in range(1,lifetime):
-            if state[track][time][0] is not None and state[track][time-1][0] is not None:
-                distance_metric=0
-                #distance_metric = (state[track][time][0]**2 - state[track][time-1][0]**2)**(0.5)
-                #+ (state[track][time][1]**2 - state[track][time-1][1]**2)**(0.5)
+    distance_metric = [0 for i in range(elements)]
+    for track in range(elements):
+        # print("Track" + str(track))
+        for time in range(1, lifetime):
+            if np.isfinite(state[track][time][0]) and np.isfinite(state[track][time - 1][0]):
+                distance_metric[track] = distance_metric[track] + ((state[track][time][0] - state[track][time - 1][0]) ** 2) ** 0.5 + ((state[track][time][1] - state[track][time - 1][1]) ** 2) ** 0.5
 
-    cost = distance_metric
+    cost = 0
+    for i in distance_metric:
+        cost = cost + i
     return cost
 
-def neighbor(state):
-    # make random change in one spots
 
-    new_state=None
-    return new_state
+# TODO: figure out how to calculate acceptance probability
+def acceptance_probability(old_cost, new_cost, T):
+    ap = math.exp(old_cost - new_cost) / T
+    return ap
 
-def acceptance_probability(old_cost,new_cost,T):
-    return 0
 
 def plot(tracks):
     # TODO: shouldn't 'Cell0000625.png' be an argument we pass in to the function? Or is this your debug code?
-    # still trying to figure out how we plot and display in python. this is just a test.
-    # plot two tracks
+
+    fig = plt.figure()
+    plt.close(fig)
+    print("I am plotting")
     for track in range(len(tracks)):
         newplot = []
-        #if not empty(tracks[track]):
+        # if not empty(tracks[track]):
         for x in range(lifetime):
             newplot.append(tracks[track][x][0])
-        plt.scatter(range(0,lifetime),newplot)
+        plt.plot(range(0, lifetime), newplot, '.-')
+
+    plt.show()
+
+    '''
+		#cellpicture = misc.imread('Cell0000625.png')
+    #plt.imshow(cellpicture)
+		# plot two tracks
+    #plt.figure(1)
+    #plt.clf()
+    #plt.plot(range(0,lifetime),newplot)
+    plt.scatter(range(0,lifetime),newplot)
+		'''
+
+
+run()
