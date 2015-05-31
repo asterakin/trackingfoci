0e5bc361e23d07c304ba1094f0f1fc84b41e78ae
asterakin <styl.stella@Gmail.com>
2015-05-31 14:42:54 -0700

    changes

--- tracking.py
+++ tracking.py
@@ -7,12 +7,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import math
 from random import random
+from operator import itemgetter
 
 MIN_SCORE = 3
 ALLOW_SPLITS = False
 ALLOW_MERGES = False
 MAX_TIME_WINDOW = 3
-
+MAX_JUMP = 5
 
 
 # returns false if all nan's
@@ -25,7 +26,7 @@ def no_nans(biglist):
 
 
 # returns spots of format
-# spots has the format Track1: [[x1,y1,score],[x2,y2,score],[x3,y4,score],... ]
+# spots has the format Track1: [[x1,y1,score,hashcode],[x2,y2,score,hashcode],[x3,y4,score,hashcode],... ]
 #  Track 2:  [[],[],[]]
 def convertMatFile(filename):
     celldata = io.matlab.loadmat(filename)
@@ -39,7 +40,7 @@ def convertMatFile(filename):
     for spot in range(elements):
         for time in range(lifetime):
             if celldata['sc'][spot][time] > MIN_SCORE:
-                spots[spot][time] = [celldata['xx'][spot][time], celldata['yy'][spot][time], celldata['sc'][spot][time]]
+                spots[spot][time] = [celldata['xx'][spot][time], celldata['yy'][spot][time], celldata['sc'][spot][time],str(time)+'_'+str(spot)]
 
     return spots
 
@@ -47,7 +48,7 @@ def convertMatFile(filename):
 def run(filename='simpleTrack.mat'):
     spots = convertMatFile(filename)
     tracks = spots.copy()
-    tracks = initial_state(tracks)
+    tracks = initial_state_2(tracks)
     plt.ion()
     plot(tracks)
     [final_state, cost] = sim_anneal(tracks)
@@ -55,65 +56,135 @@ def run(filename='simpleTrack.mat'):
     print("THE END")
 
 
+
+def distance (pointA, pointB):
+    print(pointA)
+    print(pointB)
+    return ((pointA[0] - pointB[0])**2 + (pointA[1] - pointB[1])**2)**0.5
+
+
+
+
+def initial_state3 (state):
+    # take random points, make random tracks towards both sides
+    # then try to connect random segments with simulated annealing
+    return state;
+
+
+
+def initial_state_2(state):
+    return state
+
 # TODO: START BY FINDING THE NEAREST NEIGHBOR
-def initial_state(tracks):
-    return tracks;  # for now just keep the random initial state
+def initial_state(state):
+    #starts = np.empty([elements,4])
+    #starts[:] = np.NAN
+
+    # from, to, distance
+    distance_map = []
+
+    itracks_starts = [list([[np.nan] for y in range(lifetime)]) for i in range(elements)]
+    itracks_ends = [list([[np.nan] for y in range(lifetime)]) for i in range(elements)]
+
+    #ends = np.empty([elements,4])
+    #ends[:] = np.NAN
+
+    # take starting points
+    for track in range(elements):
+        if np.isfinite(state[track][0][0]):
+            itracks_starts[track][0] = state[track][0]
+            itracks_ends[track][0] = state[track][lifetime-1]
+
+
+
+    for time in range(0, lifetime-1):
+        for track1 in range(elements):
+            pointA = itracks_starts[track1][time]
+            for track2 in range(elements):
+                pointB = state[track2][time+1]
+                if np.isfinite(pointA[0]) and np.isfinite(pointB[0]):
+                    d = distance(pointA,pointB)
+                    distance_map.append([track1,track2,d])
+
+
+        # find min - not a good check should prob use a different one.
+        while distance_map !=[]:
+            minDistance = float("inf");
+            for x in range(len(distance_map)):
+                if distance_map[x][2] < minDistance:
+                    mintrack1 = distance_map[x][0]
+                    mintrack2 = distance_map[x][1]
+                    minDistance = distance_map[x][2]
+
+            itracks_starts[mintrack1][time+1] = state[mintrack2][time+1]
+            #print(itracks_starts)
+        # remove elements for distance_map
+            j=0
+            while j < len(distance_map):
+                if distance_map[j][0] == mintrack1 or distance_map[j][1] == mintrack2:
+                    del distance_map[j]
+                else:
+                    j=j+1
+
+
+    plot (itracks_starts)
+    return state;  # for now just keep the random initial state
 
 
 def sim_anneal(state):
     old_cost = cost(state)
-    T = 10.0
+    T = 1.0
     T_min = 0.00001
     alpha = 0.9
     while T > T_min:
         i = 1
-        while i <= 100:
+        while i <= 10000:
             new_state = neighbor(state)
             new_cost = cost(state)
             ap = acceptance_probability(old_cost, new_cost, T)
-            print(str(old_cost) + " vs " + str(new_cost))
-
+            #print(str(old_cost) + ' vs ' + str(new_cost))
             if ap > random():
                 print("Accepted")
                 state = new_state
                 old_cost = new_cost
-                plot(new_state)
+                #plot(new_state)
             i += 1
             T = T * alpha
 
-    return state, cost  # TODO: calculate a neighbor state
+    return state, old_cost  # TODO: calculate a neighbor state
 
 # TODO : Figure out the non-nan values and use only those
 def neighbor(state):
     # make random change in random number of spots
+    # swap random range
+
     how_many_spots = randint(0, 10)
 
-    for spots in range(how_many_spots):
-        timepoint = randint(0, lifetime - 1)
-        track1 = randint(0, 1)  # should be elements-1 but there are too many nan tracks
-        track2 = randint(0, 1)
-        # exchange track1 with track2
-        temp = state[track1][timepoint]
-        state[track1][timepoint] = state[track2][timepoint]
-        state[track2][timepoint] = temp
+    #for spots in range(how_many_spots):
+    timepoint = randint(0, lifetime - 1)
+    track1 = randint(0, 1)  # should be elements-1 but there are too many nan tracks
+    track2 = randint(0, 1)
+    temp = state[track1][timepoint:timepoint+how_many_spots]
+    state[track1][timepoint:timepoint+how_many_spots] = state[track2][timepoint:timepoint+how_many_spots]
+    state[track2][timepoint:timepoint+how_many_spots] = temp
+    #plot(state)
 
     return state
 
 
 def cost(state):
+
     distance_metric = [0 for i in range(elements)]
     for track in range(elements):
         # print("Track" + str(track))
         for time in range(0, lifetime):
             if time > 0 and np.isfinite(state[track][time][0]) and np.isfinite(state[track][time - 1][0]):
                 distance_metric[track] = distance_metric[track] +(((state[track][time][0] - state[track][time - 1][0]) ** 2) + ((state[track][time][1] - state[track][time - 1][1]) ** 2)) ** 0.5
-            if time +1 < lifetime and np.isfinite(state[track][time][0]) and np.isfinite(state[track][time + 1][0]):
-                distance_metric[track] = distance_metric[track] +(((state[track][time][0] - state[track][time + 1][0]) ** 2) + ((state[track][time][1] - state[track][time + 1][1]) ** 2)) ** 0.5
 
-    cost = 0
+    icost = 0
     for i in distance_metric:
-        cost = cost + i
-    return cost
+        icost = icost + i
+    return icost
 
 
 # TODO: figure out how to calculate acceptance probability
@@ -125,8 +196,8 @@ def acceptance_probability(old_cost, new_cost, T):
 def plot(tracks):
     # TODO: shouldn't 'Cell0000625.png' be an argument we pass in to the function? Or is this your debug code?
 
-    fig = plt.figure()
-    plt.close(fig)
+    #fig = plt.figure()
+    plt.clf()
     print("I am plotting")
     for track in range(len(tracks)):
         newplot = []
